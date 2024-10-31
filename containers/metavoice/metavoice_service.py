ec8cadc5d3a5527ff7c2fb4029dc802ae35ac060
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2024-10-31 12:38:21 +0000

    fix metavoice by adding uuid (refernence is cached otherwise)

--- containers/metavoice/metavoice_service.py
+++ containers/metavoice/metavoice_service.py
@@ -4,6 +4,7 @@ from fastapi.responses import FileResponse
 import shutil
 import os
 import sys
+import uuid
 
 sys.path.append('/app/metavoice-src')
 
@@ -44,10 +45,12 @@ def synthesize(
     output_path = "/results_metavoice/output.wav"
     # Copy the generated wav file to the output path
     shutil.copy(wav_file_path, output_path)
+    shutil.remove(wav_file_path)
     return FileResponse(output_path, media_type="audio/wav", filename="output.wav")
 
 def process_speaker_reference(speaker_wav_bytes: bytes) -> str:
-    speaker_wav_path = "/app/speaker.wav"
+    ref_id = str(uuid.uuid4())  # Generate a unique ID for the reference
+    speaker_wav_path = f"/app/{ref_id}.wav"
     with open(speaker_wav_path, "wb") as f:
         f.write(speaker_wav_bytes)
     return speaker_wav_path
